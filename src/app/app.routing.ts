2b65d2eb4c721415f7f3c87220eaee9209de919e
LeandroDosSantosPereira <leandro.pereira@novaandradina.org>
2022-03-28 21:53:02 -0400

    create(blog page)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import { ComponentsComponent } from './components/components.component';
 import { LandingComponent } from './examples/landing/landing.component';
 import { LoginComponent } from './examples/login/login.component';
 import { ProfileComponent } from './examples/profile/profile.component';
+import { BlogComponent } from './examples/blog/blog.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
 const routes: Routes =[
@@ -15,7 +16,8 @@ const routes: Routes =[
     { path: 'nucleoicons',          component: NucleoiconsComponent },
     { path: 'examples/landing',     component: LandingComponent },
     { path: 'examples/login',       component: LoginComponent },
-    { path: 'examples/profile',     component: ProfileComponent }
+    { path: 'examples/profile',     component: ProfileComponent },
+    { path: 'examples/blog',     component:  BlogComponent}
 ];
 
 @NgModule({
